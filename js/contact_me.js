1b500026dee290dfcc17b925939b4b1d35a50ae0
Usama-Tahir <14beseutahir@seecs.edu.pk>
2019-07-27 21:34:27 +0500

    add contact end point

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "././mail/contact_me.php",
+        url: "https://us-central1-merlins-contact-form.cloudfunctions.net/contact",
         type: "POST",
         data: {
           name: name,
